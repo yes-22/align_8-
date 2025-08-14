3937cc1427b8ce11f0b4a319f0b4ace9774d85e9
yes-22 <155824945+yes-22@users.noreply.github.com>
2025-08-14 18:10:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 
 import sys
 
-if __name__ = '__main__':
+if __name__ == '__main__':
    filename = sys.argv[1]
+   data = load_ints(filename)
+   selection_sort(data)          
+   print(*data)   
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+def load_ints(path):
+    nums = []
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f:
+            for tok in line.split():
+                try:
+                    nums.append(int(tok))
+                except ValueError:
+                    pass
+    return nums
